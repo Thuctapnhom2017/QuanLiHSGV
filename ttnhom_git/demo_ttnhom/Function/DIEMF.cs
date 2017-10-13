899ff16d443b8f15e546d5847069fb6244f426c5
dodatdi <30981111+dodatdi@users.noreply.github.com>
2017-10-14 06:23:50 +0800

    Update Function Điểm

--- ttnhom_git/demo_ttnhom/Function/DIEMF.cs
+++ ttnhom_git/demo_ttnhom/Function/DIEMF.cs
@@ -10,6 +10,68 @@ namespace demo_ttnhom.Function
 {
     class DIEMF
     {
-       
+        private MyDbContext context;
+
+        public DIEMF()
+        {
+            context = new MyDbContext();
+        }
+        //trả về toàn bộ dữ liệu
+        public IQueryable<DIEM> DIEMs
+        {
+            get { return context.DIEMs; }
+        }
+
+        //trả về 1 đối tượng sản phẩm
+
+        public DIEM FindEntity(int ID)
+        {
+            DIEM dbEntry = context.DIEMs.Find(ID);
+            return dbEntry;
+
+        }
+
+        public bool Insert(DIEM model)
+        {
+            DIEM dbEntry = context.DIEMs.Find(model.ID);
+            if (dbEntry != null) return false;
+            context.DIEMs.Add(model);
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool Update(DIEM model)
+        {
+            DIEM dbEntry = context.DIEMs.Find(model.ID);
+            if (dbEntry == null) return false;
+            dbEntry.mahs=model.mahs;
+            dbEntry.mamon = model.mamon;
+            dbEntry.diemtb = model.diemtb;
+            dbEntry.hanhkiem = model.hanhkiem;
+            dbEntry.hocki = model.hocki;
+            dbEntry.namhoc = model.namhoc;
+            // context.Entry(dbEntry).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(DIEM model)
+        {
+            DIEM dbEntry = context.DIEMs.Find(model.ID);
+            if (dbEntry == null) return false;
+            //context.Entry(model).State = System.Data.Entity.EntityState.Deleted;
+            context.DIEMs.Remove(dbEntry);
+            context.SaveChanges();
+            return true;
+        }
+        public DIEM FindDiem(int hocki, string namhoc, int mahs, int mamon)
+        {
+            var query = from x in context.DIEMs
+                        where x.hocki == hocki && x.namhoc == namhoc && x.mahs == mahs && x.mamon == mamon
+                        select x;
+
+            List<DIEM> ans=query.ToList();
+            return ans.FirstOrDefault();
+        }
     }
 }
