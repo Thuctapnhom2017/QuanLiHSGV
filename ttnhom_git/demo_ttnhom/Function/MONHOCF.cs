47ff08a071b5c1546ff2f0f9a61a7edc8fce76fd
Ndhswat <32015041+Ndhswat@users.noreply.github.com>
2017-10-14 06:44:35 +0700

    funtion mon hoc

--- ttnhom_git/demo_ttnhom/Function/MONHOCF.cs
+++ ttnhom_git/demo_ttnhom/Function/MONHOCF.cs
@@ -9,6 +9,59 @@ namespace demo_ttnhom.Function
 {
     class MONHOCF
     {
-       
+        private MyDbContext context;
+
+        public MONHOCF()
+        {
+            context = new MyDbContext();
+        }
+        //trả về toàn bộ dữ liệu
+        public IQueryable<MONHOC> MONHOCs
+        {
+            get { return context.MONHOCs; }
+        }
+
+        //trả về 1 đối tượng sản phẩm
+
+        public MONHOC FindEntity(int ID)
+        {
+            MONHOC dbEntry = context.MONHOCs.Find(ID);
+            return dbEntry;
+
+        }
+
+        public bool Insert(MONHOC model)
+        {
+            MONHOC dbEntry = context.MONHOCs.Find(model.ID);
+            if (dbEntry != null) return false;
+            context.MONHOCs.Add(model);
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool Update(MONHOC model)
+        {
+            MONHOC dbEntry = context.MONHOCs.Find(model.ID);
+            if (dbEntry == null) return false;
+            dbEntry.tenmon = model.tenmon;
+            dbEntry.note = model.note;
+            // context.Entry(dbEntry).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(MONHOC model)
+        {
+            MONHOC dbEntry = context.MONHOCs.Find(model.ID);
+            if (dbEntry == null) return false;
+            //context.Entry(model).State = System.Data.Entity.EntityState.Deleted;
+            context.MONHOCs.Remove(dbEntry);
+            context.SaveChanges();
+            return true;
+        }
+        public List<MONHOC> Search(string key)
+        {
+            return new MONHOCF().MONHOCs.Where(x => x.ID.ToString().Contains(key) || x.tenmon.Contains(key) || x.note.Contains(key) ).ToList();
+        }
     }
 }
