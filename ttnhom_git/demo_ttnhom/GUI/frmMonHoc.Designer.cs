292e2b9491303212ae65b67d6b4e987c1da83fde
Ndhswat <32015041+Ndhswat@users.noreply.github.com>
2017-10-14 22:01:18 +0700

    da xong form mon hoc nhe

--- ttnhom_git/demo_ttnhom/GUI/frmMonHoc.Designer.cs
+++ ttnhom_git/demo_ttnhom/GUI/frmMonHoc.Designer.cs
@@ -28,19 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.GridMon = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNote = new DevExpress.XtraEditors.MemoEdit();
+            this.btnLuu = new DevExpress.XtraEditors.SimpleButton();
+            this.btnXoa = new DevExpress.XtraEditors.SimpleButton();
+            this.btnThem = new DevExpress.XtraEditors.SimpleButton();
+            this.txtName = new DevExpress.XtraEditors.TextEdit();
+            this.txtID = new DevExpress.XtraEditors.TextEdit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.panelControl3 = new DevExpress.XtraEditors.PanelControl();
+            this.txtTimKiem = new DevExpress.XtraEditors.TextEdit();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.GridMon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNote.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).BeginInit();
+            this.panelControl3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTimKiem.Properties)).BeginInit();
             this.SuspendLayout();
+            // 
+            // GridMon
+            // 
+            this.GridMon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GridMon.Location = new System.Drawing.Point(2, 57);
+            this.GridMon.MainView = this.gridView1;
+            this.GridMon.Name = "GridMon";
+            this.GridMon.Size = new System.Drawing.Size(583, 432);
+            this.GridMon.TabIndex = 1;
+            this.GridMon.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn3});
+            this.gridView1.GridControl = this.GridMon;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.ReadOnly = true;
+            this.gridView1.OptionsDetail.DetailMode = DevExpress.XtraGrid.Views.Grid.DetailMode.Default;
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "ID";
+            this.gridColumn1.FieldName = "ID";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            this.gridColumn1.Width = 71;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "Tên Môn Học";
+            this.gridColumn2.FieldName = "tenmon";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
+            this.gridColumn2.Width = 194;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "Ghi Chú";
+            this.gridColumn3.FieldName = "note";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 2;
+            this.gridColumn3.Width = 300;
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(this.label3);
+            this.panelControl2.Controls.Add(this.txtNote);
+            this.panelControl2.Controls.Add(this.btnLuu);
+            this.panelControl2.Controls.Add(this.btnXoa);
+            this.panelControl2.Controls.Add(this.btnThem);
+            this.panelControl2.Controls.Add(this.txtName);
+            this.panelControl2.Controls.Add(this.txtID);
+            this.panelControl2.Controls.Add(this.label2);
+            this.panelControl2.Controls.Add(this.label1);
+            this.panelControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl2.Location = new System.Drawing.Point(587, 0);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(333, 491);
+            this.panelControl2.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Ghi Chú";
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(108, 111);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(202, 100);
+            this.txtNote.TabIndex = 19;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(232, 301);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 18;
+            this.btnLuu.Text = "Lưu";
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(122, 301);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 17;
+            this.btnXoa.Text = "Xóa";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(12, 301);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 16;
+            this.btnThem.Text = "Thêm";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(108, 68);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(202, 20);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(108, 19);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(202, 20);
+            this.txtID.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên Môn Học";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Môn Học";
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.GridMon);
+            this.panelControl1.Controls.Add(this.panelControl3);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(587, 491);
+            this.panelControl1.TabIndex = 17;
+            // 
+            // panelControl3
+            // 
+            this.panelControl3.Controls.Add(this.txtTimKiem);
+            this.panelControl3.Controls.Add(this.label9);
+            this.panelControl3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl3.Location = new System.Drawing.Point(2, 2);
+            this.panelControl3.Name = "panelControl3";
+            this.panelControl3.Size = new System.Drawing.Size(583, 55);
+            this.panelControl3.TabIndex = 0;
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(127, 17);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(404, 20);
+            this.txtTimKiem.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(22, 20);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Tìm Kiếm";
             // 
             // frmMonHoc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(920, 491);
+            this.Controls.Add(this.panelControl2);
+            this.Controls.Add(this.panelControl1);
             this.Name = "frmMonHoc";
             this.Text = "frmMonHoc";
+            ((System.ComponentModel.ISupportInitialize)(this.GridMon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            this.panelControl2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNote.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).EndInit();
+            this.panelControl3.ResumeLayout(false);
+            this.panelControl3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTimKiem.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private DevExpress.XtraGrid.GridControl GridMon;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraEditors.SimpleButton btnLuu;
+        private DevExpress.XtraEditors.SimpleButton btnXoa;
+        private DevExpress.XtraEditors.SimpleButton btnThem;
+        private DevExpress.XtraEditors.TextEdit txtName;
+        private DevExpress.XtraEditors.TextEdit txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.PanelControl panelControl3;
+        private DevExpress.XtraEditors.TextEdit txtTimKiem;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label3;
+        private DevExpress.XtraEditors.MemoEdit txtNote;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
     }
 }
--- ttnhom_git/demo_ttnhom/GUI/frmMonHoc.cs
+++ ttnhom_git/demo_ttnhom/GUI/frmMonHoc.cs
@@ -18,6 +18,107 @@ namespace demo_ttnhom.GUI
         public frmMonHoc()
         {
             InitializeComponent();
-        } 
+            
+            Init_();
+            Bindingtxt();
+            PhanQuyen();
+        }
+        private void PhanQuyen()
+        {
+            if (Constant.NGUOIDUNG_ != null)
+            {
+                if (Constant.NGUOIDUNG_.Admin == true)
+                {
+                    btnLuu.Enabled = btnXoa.Enabled = btnThem.Enabled = true;
+                }
+                else
+                {
+                    btnLuu.Enabled = btnXoa.Enabled = btnThem.Enabled = false;
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Init_()
+        {
+
+            BindingSource source = new BindingSource();
+            source.DataSource = new MONHOCF().MONHOCs.ToList();
+            GridMon.DataSource = source;
+        }
+        private void Bindingtxt()
+        {
+            txtID.DataBindings.Add(new Binding("Text", GridMon.DataSource, "ID", true, DataSourceUpdateMode.Never));
+            txtName.DataBindings.Add(new Binding("Text", GridMon.DataSource, "tenmon", true, DataSourceUpdateMode.Never));
+            txtNote.DataBindings.Add(new Binding("Text", GridMon.DataSource, "note", true, DataSourceUpdateMode.Never));
+        }
+        private void clear()
+        {
+            txtID.Text = txtName.Text = txtNote.Text = "";
+        }
+        private void ClearBinding()
+        {
+            txtID.DataBindings.Clear();
+            txtName.DataBindings.Clear();
+            txtNote.DataBindings.Clear();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Bạn có muốn xóa nhà cung cấp " + txtName.Text + " không?", "Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    var ans = new MONHOCF().Delete(new MONHOC { ID = int.Parse(txtID.Text) });
+                    Init_(); ClearBinding(); Bindingtxt();
+                }
+            }
+            catch { }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MONHOC model = new MONHOC()
+                {
+                    tenmon = txtName.Text,
+                    note = txtNote.Text,
+                };
+                int ID_ = -1;
+                if (txtID.Text != "") ID_ = int.Parse(txtID.Text.ToString());
+                model.ID = ID_;
+                if (txtID.Text == "")
+                {
+                    var result = new MONHOCF().Insert(model);
+                    if (result) MessageBox.Show("Done"); else MessageBox.Show("Lỗi");
+                }
+                else
+                {
+                    var result = new MONHOCF().Update(model);
+                    if (result) MessageBox.Show("Done"); else MessageBox.Show("Lỗi");
+                }
+                Init_(); ClearBinding(); Bindingtxt();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void txtTimKiem_EditValueChanged(object sender, EventArgs e)
+        {
+            GridMon.DataSource = new MONHOCF().Search(txtTimKiem.Text);
+            ClearBinding(); Bindingtxt();
+        }
     }
 }
